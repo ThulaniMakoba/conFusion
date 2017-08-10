cc9a0483bea25d1c2c14a77b0dfcd4215cd886cc
makhobast@gmail.com <makhobast@gmail.com>
2017-08-10 16:10:45 +0200

    angular components part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
-=======
-import { NgModule } from '@angular/core';
->>>>>>> bd62b35faee6c72a38a978e581360244e13a9d70
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-<<<<<<< HEAD
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule
-=======
-    BrowserModule
->>>>>>> bd62b35faee6c72a38a978e581360244e13a9d70
   ],
   providers: [],
   bootstrap: [AppComponent]
